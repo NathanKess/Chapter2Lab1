2c87e9061b118907c86a11b217f6e00c6253ab34
NathanKess <31323160+NathanKess@users.noreply.github.com>
2017-10-10 11:22:26 -0700

    Initial commit

---
+++ TableofStudentGrades.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class TableofStudentGrades here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class TableofStudentGrades
+{
+    public static void main (String[] args)
+        {
+         System.out.println ("-----------------------------------------");
+         System.out.println ("\t==\tStudent Points\t==");
+         System.out.println ("-----------------------------------------");
+         System.out.println ("Name\t\tLab\tBonus\tTotal");
+         System.out.println ("----\t\t---\t-----\t-----");
+         System.out.println ("Joe\t\t43\t7\t50");
+         System.out.println ("William\t\t50\t8\t58");
+         System.out.println ("Mary Sue\t39\t10\t49");
+         System.out.println ("Noor\t\t-1\t1\t0");
+         System.out.println ("Parker\t\t11/12\t0\tDrop Out");
+        }
+}
